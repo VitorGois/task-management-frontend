87fc820bd74e83443fe9897d586b38230efb71b2
VitorGois <VitorGois@users.noreply.github.com>
2021-08-01 01:19:46 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.0608b84be0d8bba7e1e8d877e32f1a50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f9f7274718c55d51e01a8213805fb6",
+    "url": "https://vitorgois.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "0dbf00a884485eaaf381",
+    "url": "https://vitorgois.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "123cb16e2108c6d80b9e",
+    "url": "https://vitorgois.github.io/task-management-frontend/static/js/2.df681a86.chunk.js"
+  },
+  {
+    "revision": "0dbf00a884485eaaf381",
+    "url": "https://vitorgois.github.io/task-management-frontend/static/js/main.dff64769.chunk.js"
+  },
+  {
+    "revision": "97ae315bb47246612a62",
+    "url": "https://vitorgois.github.io/task-management-frontend/static/js/runtime~main.0db82c08.js"
+  }
+]);
